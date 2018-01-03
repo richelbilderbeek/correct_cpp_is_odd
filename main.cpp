236970a271bf5571454baa420d8560199c580462
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-03 12:53:14 +0100

    EOD

--- main.cpp
+++ main.cpp
@@ -1,54 +1,18 @@
-#include <iostream>
 #include <string>
-#include <vector>
+#include <iostream>
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) 
 {
-  if (argc != 2) return 1;
+  if (argc == 1) return 1;
   try
   {
-    const int i{std::stoi(argv[1])};
-    if (i == 0)
-    {
-      std::cout << "false\n";
-      return 0;
-    }
-    //Collect the proper divisors
-    std::vector<int> v;
-    if (i < 2)
-    {
-      //v is okay as it is
-    }
-    else if (i == 2)
-    {
-      v.push_back(1);
-    }
-    else
-    {
-      for (int j=1; j!=i-1; ++j)
-      {
-        if (i % j == 0)
-        {
-          v.push_back(j);
-        }
-      }
-    }
-    //sum the proper divisors
-    int sum{0};
-    for (const int j: v) { sum += j; }
-    //is it perfect?
-    const bool is_perfect{sum == i};
-    //show
-    if (is_perfect)
-    {
-      std::cout << "true\n";
-    }
-    else
-    {
-      std::cout << "false\n";
-    }
+    std::cout << std::boolalpha << (std::stoi(argv[1]) % 2 == 1) << '\n';
   }
-  catch (const std::exception&)
+  catch (const std::invalid_argument&)
+  {
+    return 1;
+  }
+  catch (const std::out_of_range&)
   {
     return 1;
   }

dc990506cf505e4c54dbad24e85d49728b71a441
CarmenIJsebaart <cijsebaart@gmail.com>
2018-01-03 20:53:57 +0100

    Did exercise is_odd

--- main.cpp
+++ main.cpp
@@ -1,4 +1,38 @@
-main(argc, argv)
-{
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+#include <string>
 
+int main(int argc, char** argv)
+{
+  if(argc == 1)
+  {
+    std::cout << "No correct input!\n";
+  }
+  try
+  {
+    if(argc != 1)
+    {
+      int number = std::stoi(argv[1]);
+      int modulo = number % 2;
+      if(modulo == 1)
+      {
+        std::cout << "true\n";
+      }
+      if(modulo == 0)
+      {
+        std::cout << "false\n";
+      }
+    }
+  }
+  catch(const std::out_of_range&)
+  {
+    std::cout << "This number is not valid!\n";
+    return 1;
+  }
+  catch(const std::invalid_argument&)
+  {
+    std::cout << "This is not a number!\n";
+    return 1;
+  }
 }

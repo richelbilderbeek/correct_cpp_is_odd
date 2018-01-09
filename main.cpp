227118d6b99848ba7c6844e79657935aac5946ed
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-09 16:11:30 +0100

    Correct C++ update

--- main.cpp
+++ main.cpp
@@ -1,19 +1,4 @@
-#include <string>
-#include <iostream>
-
-int main(int argc, char* argv[]) 
+main(argc, argv)
 {
-  if (argc == 1) return 1;
-  try
-  {
-    std::cout << std::boolalpha << (std::stoi(argv[1]) % 2 != 0) << '\n';
-  }
-  catch (const std::invalid_argument&)
-  {
-    return 1;
-  }
-  catch (const std::out_of_range&)
-  {
-    return 1;
-  }
+
 }

88ac284ff5c2f8f412aa56374d27ddbccd25617b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-18 10:01:12 +0200

    Correct C++ update

--- main.cpp
+++ main.cpp
@@ -1,33 +1,4 @@
-/** @file */
-/** @file */
-/** @file */
-/** @file */
-/** @file */
-/** @file */
-/** @file */
-/** @file */
-/** @file */
-/** @file */
-/** @file */
-/** @file */
-/** @file */
-#include <string>
-#include <iostream>
-
-/// is_odd main function
-int main(int argc, char* argv[]) 
+main(argc, argv)
 {
-  if (argc == 1) return 1;
-  try
-  {
-    std::cout << std::boolalpha << (std::stoi(argv[1]) % 2 != 0) << '\n';
-  }
-  catch (const std::invalid_argument&)
-  {
-    return 1;
-  }
-  catch (const std::out_of_range&)
-  {
-    return 1;
-  }
+
 }
